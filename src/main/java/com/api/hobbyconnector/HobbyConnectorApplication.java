1e97b799cc233e9e67be4273fa395d2eddb6f9ef
TrascaianNicolae98 <tnae0095@scs.ubbcluj.ro>
2020-11-08 17:43:05 +0200

    begin

---
+++ src/main/java/com/api/hobbyconnector/HobbyConnectorApplication.java
@@ -0,0 +1,13 @@
+package com.api.hobbyconnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbyConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HobbyConnectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/hobbyconnector/HobbyConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.hobbyconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbyConnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
