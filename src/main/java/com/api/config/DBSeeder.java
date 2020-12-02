df0acd5349fb64d4368a4bf18decea8a8373427a
TrascaianNicolae98 <tnae0095@scs.ubbcluj.ro>
2020-12-02 15:29:31 +0200

    championship seed

--- src/main/java/com/api/config/DBSeeder.java
+++ src/main/java/com/api/config/DBSeeder.java
@@ -1,9 +1,12 @@
 package com.api.config;
 
+import com.api.entities.Championship;
 import com.api.entities.ConnectionType;
 import com.api.entities.Hobby;
+import com.api.repository.ChampionshipRepository;
 import com.api.repository.ConnectionTypeRepository;
 import com.api.repository.HobbyRepository;
+import java.util.ArrayList;
 import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -12,6 +15,7 @@ import org.springframework.stereotype.Component;
 public class DBSeeder {
   @Autowired private ConnectionTypeRepository connectionTypeRepository;
   @Autowired private HobbyRepository hobbyRepository;
+  @Autowired private ChampionshipRepository championshipRepository;
 
   public DBSeeder() {}
 
@@ -73,5 +77,56 @@ public class DBSeeder {
     this.hobbyRepository.save(hobby4);
     this.hobbyRepository.save(hobby5);
     this.hobbyRepository.save(hobby7);
+  }
+
+  @PostConstruct
+  public void seedChampionships() {
+    Championship championship1 =
+        new Championship(
+            "Football",
+            "https://images.unsplash.com/flagged/photo-1571771710019-ca58cf80f225?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+            "link2",
+            new ArrayList<>());
+
+    Championship championship2 =
+        new Championship(
+            "Basket",
+            "https://images.unsplash.com/photo-1467809941367-bbf259d44dd6?ixlib=rb-1.2.1&auto=format&fit=crop&w=1190&q=80",
+            "link2",
+            new ArrayList<>());
+
+    Championship championship3 =
+        new Championship(
+            "Paintball",
+            "https://images.unsplash.com/photo-1522499820455-9c821da82b9e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+            "link2",
+            new ArrayList<>());
+
+    Championship championship4 =
+        new Championship(
+            "Handball",
+            "https://www.athens2020.org/sites/default/files/styles/fullhd/public/disciplines-handball-ball.jpg?itok=wtwhHVEJ",
+            "link2",
+            new ArrayList<>());
+
+    Championship championship5 =
+        new Championship(
+            "Chess",
+            "https://images.unsplash.com/photo-1523875194681-bedd468c58bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
+            "link2",
+            new ArrayList<>());
+
+    Championship championship6 =
+        new Championship(
+            "Volleyball",
+            "https://images.unsplash.com/photo-1562552052-c72ceddf93dc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+            "link2",
+            new ArrayList<>());
+    this.championshipRepository.save(championship1);
+    this.championshipRepository.save(championship2);
+    this.championshipRepository.save(championship3);
+    this.championshipRepository.save(championship4);
+    this.championshipRepository.save(championship5);
+    this.championshipRepository.save(championship6);
   }
 }
